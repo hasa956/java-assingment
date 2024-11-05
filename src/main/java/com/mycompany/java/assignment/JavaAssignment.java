ca7ed0f59c061972d8a99af475666828d9dfafa9
hasa956 <140059796+hasa956@users.noreply.github.com>
2024-11-05 18:58:11 +0800

    commit 1

---
+++ src/main/java/com/mycompany/java/assignment/JavaAssignment.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java.assignment;
+
+
+public class JavaAssignment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
